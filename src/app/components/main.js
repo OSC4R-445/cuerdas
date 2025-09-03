b51cd4ebafe927055d6d1690329f062041edae6c
OSC4R <oscardanieliafratelopez@gmail.com>
2025-09-03 15:03:03 -0400

    Refactor Main and Card components for improved structure and styling

--- src/app/components/main.js
+++ src/app/components/main.js
@@ -1,63 +1,82 @@
 import Image from "next/image";
 import Crear from "./crear";
 
-export default function Main() {
-  const estaLogueado = true;
-  const Post1 = {
-    nameAccount: "Dolar Today",
-    linkFeed: "https://.com/post/1",
-    description: "dolartoday.com/que-s...",
-    imgFeed: "/img/dolarTodayPost.png",
-    likes: 0,
-    timestamp: 120394072487,
-  };
-  const Post2 = {
-    nameAccount: "Real Lawfirm",
-    linkFeed: "ttps://.com/post/2",
-    description: "We've settled over 2000 cases...",
-    imgFeed: "",
-    likes: 0,
-    timestamp: 120394802938,
-  };
-
+const Card = ({ post }) => {
   return (
-    <main>
-      <Crear />
+    <div className="card">
       <div className="postComment">
         <Image
-          src="/img/dolarTodayPfp.png"
+          src={post.profileImg}
           width={50}
           height={50}
-          alt="pfp"
+          alt={`Profile of ${post.nameAccount}`}
           className="pfpMain"
         />
         <div>
-          <div>Dolar Today</div>
-          <button type="button" className="btn btn-link">
-            dolartoday.com/que-s...
-          </button>
+          <div>{post.nameAccount}</div>
+          <p>{post.description}</p>
         </div>
         <div>
-          <div>3 min</div>
+          <div>{post.timestamp}</div>
         </div>
         <i className="bi bi-three-dots"></i>
       </div>
+
       <div className="postContent">
-        <Image
-          src="/img/dolarTodayPost.png"
-          width={500}
-          height={500}
-          alt="Dolar Today Post"
-          className="Post"
-        />
+        {post.imgFeed && (
+          <Image
+            src={post.imgFeed}
+            width={500}
+            height={500}
+            alt={`Post from ${post.nameAccount}`}
+            className="Post"
+          />
+        )}
       </div>
+
       <div className="postFooter">
         <h4 className="bi bi-heart"></h4>
         <h4 className="bi bi-chat-right"></h4>
         <h4 className="bi bi-arrow-repeat"></h4>
         <h4 className="bi bi-send"></h4>
       </div>
-      <div className="postLikes">1 Me gusta</div>
+
+      <div className="postLikes">{post.likes} Me gusta</div>
+    </div>
+  );
+};
+
+export default function Main() {
+  const estaLogueado = true
+
+  const posts = [
+    {
+      nameAccount: "Dolar Today",
+      profileImg: "/img/dolarTodayPfp.png",
+      linkFeed: "https://.com/Posts/1",
+      description: "dolartoday.com/que-s...",
+      imgFeed: "/img/dolarTodayPost.png",
+      likes: 125,
+      timestamp: "3 min",
+      id: 1,
+    },
+    {
+      nameAccount: "Real Lawfirm",
+      profileImg: "/img/dolarTodayPfp.png",
+      linkFeed: "https://.com/Posts/2",
+      description: "We've settled over 2000 cases...",
+      imgFeed: "/img/dolarTodayPost.png",
+      likes: 30,
+      timestamp: "10 min",
+      id: 2,
+    }
+  ];
+
+  return (
+    <main>
+      <Crear />
+      <Card key={posts[0].id} post={posts[0]} />
+      {estaLogueado && <Card key={posts[1].id} post={posts[1]} />}
     </main>
   );
 }
